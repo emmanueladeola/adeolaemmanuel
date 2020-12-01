e30ea3ef0f0527abb4cb195108827276d5d05cb3
Adeolaemmanuel <adeolaemmanuel006@gmail.com>
2020-12-02 00:06:52 +0100

    Modified files

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
 });
 
 app.listen(process.env.PORT || 1996);
